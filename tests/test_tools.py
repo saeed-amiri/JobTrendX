510156fe3d7bc3a310781e0a7ff16760be6e357d
saeed amiri <sdramiri952@gmail.com>
2025-01-28 16:27:45 +0100

    test for checking if the dir is not empty

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -9,7 +9,7 @@ from unittest.mock import patch
 
 import pytest
 
-from jobtrendx.tools import check_directory
+from jobtrendx.tools import check_directory, check_dir_not_empty
 
 
 def test_check_directory_exists() -> None:
@@ -23,5 +23,20 @@ def test_check_directory_not_exists() -> None:
     with patch.object(Path, 'exists', return_value=False):
         with pytest.raises(SystemExit) as exit_info:
             check_directory('test_dir')
+        assert exit_info.type == SystemExit
+        assert exit_info.value.code == 1
+
+
+def test_check_dir_not_empty_contains() -> None:
+    """Test if the check_dir_not_empty knows dir is not empty"""
+    with patch.object(Path, 'iterdir', return_value=[1, 2, 3]):
+        assert check_dir_not_empty('test_dir')
+
+
+def test_check_dir_not_empty_not_contains() -> None:
+    """Test if the check_dir_not_empty knows dir is empty"""
+    with patch.object(Path, 'iterdir', return_value=[]):
+        with pytest.raises(SystemExit) as exit_info:
+            check_dir_not_empty('test_dir')
         assert exit_info.type == SystemExit
         assert exit_info.value.code == 1
