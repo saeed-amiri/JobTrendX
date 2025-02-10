7756624be45ca15b382fee8672d06e4ce96b9846
saeed amiri <sdramiri952@gmail.com>
2025-02-10 16:24:21 +0100

    update to get the source from config

--- src/jobtrendx/main.py
+++ src/jobtrendx/main.py
@@ -2,15 +2,23 @@
 The main entry point of the application.
 """
 # pylint: disable=no-value-for-parameter
+
 import hydra
 from omegaconf import DictConfig
+
+from . import email_processor
+from . import logger
+
+
+LOG: logger.logging.Logger = logger.setup_logger('jobtrendx.log')
 
 
 @hydra.main(config_path="conf", config_name="config", version_base=None)
 def main(cfg: DictConfig) -> None:
     # pylint: disable=missing-function-docstring
     # pylint: disable=unused-argument
-    print(hydra.compose("config"))
+    src: str = cfg.defaults.paths.emails
+    email_processor.EmailProcessor(email_dir=src, log=LOG)
 
 
 if __name__ == "__main__":
