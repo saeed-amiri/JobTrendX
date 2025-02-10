ce80411a9c444bc1c5489dd46cb304b7dfb28893
saeed amiri <sdramiri952@gmail.com>
2025-02-10 15:38:32 +0100

    return the dict of the emails` content

--- src/jobtrendx/email_processor.py
+++ src/jobtrendx/email_processor.py
@@ -3,6 +3,10 @@ Reading emails frpm .eml files and extracting the body of the email
 """
 
 import sys
+from pathlib import Path
+
+import email
+
 from . import tools
 from . import logger
 from . import colors_text as ct
@@ -28,8 +32,10 @@ class EmailProcessor:
         tools.check_dir_not_empty(self.email_dir)
         all_files: list[str] = tools.returns_all_files_in_dir(self.email_dir)
         eml_files: list[str] = tools.returns_eml_files(all_files, 'eml')
-        eml_paths: list[str] = tools.returns_eml_path(self.email_dir,
-                                                      eml_files)
+        eml_paths: list[Path] = tools.returns_eml_path(self.email_dir,
+                                                       eml_files)
+        emails_dict: dict[Path, "email.message.EmailMessage"] = \
+            tools.returns_email_contant(eml_paths=eml_paths)
 
 
 if __name__ == "__main__":
