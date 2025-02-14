f85352f4b2c1126791a82501656dd5125d9d0e07
saeed amiri <sdramiri952@gmail.com>
2025-02-14 16:59:16 +0100

    add: initiate analying

---
+++ src/jobtrendx/analysis.py
@@ -0,0 +1,27 @@
+"""Analysis the email contents and order them in tabels"""
+
+from pathlib import Path
+
+import email
+from email.message import EmailMessage
+
+from . import logger
+from . import tools
+
+
+class AnalysisEmails:
+    """Analysing the emails"""
+
+    __slots__ = []
+
+    def __init__(self,
+                 eml_dict: dict[Path, "email.message.EmailMesagge"],
+                 log: logger.logging.Logger
+                 ) -> None:
+        self.analysing_object(eml_dict)
+
+    def analysing_object(self,
+                         eml_dict: dict[Path, "email.message.EmailMesagge"],
+                         ) -> None:
+        """initiate the analysis"""
+        tools.extract_email_detail(eml_dict)
