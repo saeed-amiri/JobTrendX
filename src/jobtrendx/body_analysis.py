b182d7b21de91721a0413d67deecd6d6f3e4f8de
saeed amiri <sdramiri952@gmail.com>
2025-02-27 14:50:40 +0100

    analysing: a separate module for analyzing the body of the email

---
+++ src/jobtrendx/body_analysis.py
@@ -0,0 +1,20 @@
+"""
+Analyzing the "body" section of the emails.
+Emails contains several sections, depends on the language of
+the email, this sections have different titles.
+This titles are set in cfg/defaults/analysis.yaml:
+
+sections:
+  job_title: ["Beliebter Job", "Top Treffer"]
+  company_info: ["Wer wir sind.", "Lloyds Bank GmbH and its brands"]
+  job_description: ["Das wird dein Job", "Your tasks"]
+  requirements: ["Das bringst du mit", "Your knowledge/experience"]
+  offer: ["Das bieten wir dir", "We offer"]
+
+This module here, first must separate the "body" text, based
+on the sections and than grep the information of each sections
+and return them.
+
+26 Feb. 2025
+Samiri
+"""
