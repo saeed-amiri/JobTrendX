9c82fa29eb8076a402b07f39da630bf1f8bf934a
saeed amiri <sdramiri952@gmail.com>
2025-04-17 19:18:16 +0200

    remove the extra function

--- src/jobtrendx/payload_analysis.py
+++ src/jobtrendx/payload_analysis.py
@@ -313,38 +313,3 @@ def _get_salary_amount(line: str) -> tuple[float | str,
             return min_salary, max_salary, unit
 
     return "Nan", "Nan", "Nan"
-
-
-def _get_sections(payload: str,
-                  sections: dict[str, str]
-                  ) -> dict[str, str]:
-    """
-    Splits the email payload into sections based on predefined
-    section titles, mapping them to standardized keys.
-
-    Args:
-        payload (str): The email payload text.
-        sections (dict[str, str]): Dictionary mapping
-        standardized keys to language-specific section titles.
-
-    Returns:
-        dict[str, str]: Dictionary with standardized section
-        keys and extracted text.
-    """
-    section_data: dict[str, str] = {key: "" for key in sections.keys()}
-    pattern: str = "|".join([re.escape(title) for title in sections.values()])
-
-    # Split payload into sections using regex
-    parts = re.split(f"({pattern})", payload)
-
-    # Iterate over parts and map them to standardized section keys
-    current_key = None
-    for part in parts:
-        part = part.strip()
-        if part in sections.values():
-            current_key = next(
-                key for key, title in sections.items() if title == part)
-        elif current_key:
-            section_data[current_key] += part + "\n"
-
-    return section_data
