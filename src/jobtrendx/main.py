040a9ae5589e55380b26704f16c7f38b774d949b
saeed amiri <sdramiri952@gmail.com>
2025-04-20 18:20:22 +0200

    clean up

--- src/jobtrendx/main.py
+++ src/jobtrendx/main.py
@@ -10,6 +10,7 @@ PYTHONPATH=src python -m jobtrendx.main defaults.paths.emails="<NEW_DIR>"
 import typing
 
 import hydra
+import pandas as pd
 from omegaconf import DictConfig
 
 from . import logger
@@ -29,13 +30,15 @@ def main(cfg: DictConfig) -> None:
     # pylint: disable=missing-function-docstring
     # pylint: disable=unused-argument
     src: str = cfg.defaults.paths.emails
+
     email_prc = email_processor.EmailProcessor(eml_dir=src, log=LOG)
     email_prc.execute()
-    eml_dict: dict[Path, "email.message.EmailMessage"] = \
-        email_prc.eml_dict
+    eml_dict: dict[Path, "email.message.EmailMessage"] = email_prc.eml_dict
+
     anlaz = analysis.AnalysisEmails(eml_dict=eml_dict, cfg=cfg)
     anlaz.analyzing(log=LOG)
     anlaz.unify_terms(log=LOG)
+    df_i: pd.DataFrame = anlaz.df_info
 
 
 if __name__ == "__main__":
