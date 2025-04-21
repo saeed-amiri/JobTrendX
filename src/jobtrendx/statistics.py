cb92602d69fc62ecdfe12592b83e811b2bb362b3
saeed amiri <sdramiri952@gmail.com>
2025-04-21 17:37:01 +0200

    initiate the analysis

---
+++ src/jobtrendx/statistics.py
@@ -0,0 +1,44 @@
+"""applying statistics on the info from the job ads
+21 Apr. 2025
+S.Amiri
+"""
+
+
+import pandas as pd
+
+from . import logger
+from . import tools_statistics as tools
+
+
+class StatisticsManager:
+    """do the math"""
+
+    __slots__: list[str] = [
+        'df_info',
+        'job_title_top'
+    ]
+
+    df_info: pd.DataFrame
+    job_title_top: pd.Series
+
+    def __init__(self,
+                 df_info: pd.DataFrame,
+                 ) -> None:
+        self.df_info = df_info
+
+    def statistics(self,
+                   log: logger.logging.Logger
+                   ) -> None:
+        """call the methods and set the objects"""
+        self._analyze_job_titles(log)
+
+    def _analyze_job_titles(self,
+                      log: logger.logging.Logger
+                      ) -> None:
+        """analyzing the job titles"""
+        summary: pd.DataFrame
+        summary, self.job_title_top = \
+            tools.anlz_titles(self.df_info['job_title'])
+
+        log.info(f'\nJob title summary:\n\t{summary}'
+                 f'\n\tTop jobs:\n\t{self.job_title_top}')
