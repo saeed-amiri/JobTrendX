45f3ae5232aa6a7c3d3ad609d1aea33c03143feb
saeed amiri <sdramiri952@gmail.com>
2025-04-21 18:09:54 +0200

    rename a variable

--- src/jobtrendx/tools_statistics.py
+++ src/jobtrendx/tools_statistics.py
@@ -8,33 +8,33 @@ __all__ = [
 ]
 
 
-def anlz_string_cols(titles: pd.Series) -> tuple[pd.DataFrame, pd.Series]:
+def anlz_string_cols(col: pd.Series) -> tuple[pd.DataFrame, pd.Series]:
     """
-    Analyze the titles of the jobs, count them, and return
+    Analyze the col of the jobs, count them, and return
     statistics.
 
     Args:
-        titles (pd.Series): A pandas Series containing job
-        titles.
+        col (pd.Series): A pandas Series containing job
+        col.
 
     Returns:
         tuple[pd.DataFrame, pd.Series]: A summary DataFrame
         with statistics and a Series with the top counts.
     """
-    titles = titles.astype(str).str.strip()
-    titles = titles.replace(['nan', 'Nan', 'None', '', None], pd.NA)
+    col = col.astype(str).str.strip()
+    col = col.replace(['nan', 'Nan', 'None', '', None], pd.NA)
 
-    total: int = len(titles)
-    missing: int = titles.isna().sum()
+    total: int = len(col)
+    missing: int = col.isna().sum()
     valids: int = total - missing
 
-    unique: int = titles.nunique(dropna=True)
-    top = titles.value_counts(dropna=True)
+    unique: int = col.nunique(dropna=True)
+    top = col.value_counts(dropna=True)
 
     summary = pd.DataFrame({
         'Total': [total],
         'Valid': [valids],
         'Missing': [missing],
-        'Unique Titles': [unique]
+        'Unique col': [unique]
     })
     return summary, top
