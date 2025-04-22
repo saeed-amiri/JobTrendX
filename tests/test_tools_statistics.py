55c72f8ca058d83c1309bd9c447f86c0fa70fda5
saeed amiri <sdramiri952@gmail.com>
2025-04-22 17:25:24 +0200

    tests for analyzing the numberical cols

--- tests/test_tools_statistics.py
+++ tests/test_tools_statistics.py
@@ -4,7 +4,8 @@ Tests for the statistics tools
 
 import unittest
 import pandas as pd
-from jobtrendx.tools_statistics import anlz_string_cols, anlz_list_cols
+from jobtrendx.tools_statistics import anlz_string_cols, anlz_list_cols, \
+    anlz_numerical_cols
 
 
 class TestAnlzTitles(unittest.TestCase):
@@ -43,7 +44,6 @@ class TestAnlzTitles(unittest.TestCase):
             "Machine Learning Engineer": 1
         }, name='count')
         pd.testing.assert_series_equal(top, expected_top)
-
 
 
 class TestAnlzListCols(unittest.TestCase):
@@ -88,6 +88,50 @@ class TestAnlzListCols(unittest.TestCase):
         }, name='count')
         pd.testing.assert_series_equal(counts, expected_counts)
 
+
+class TestAnlzNumericalCols(unittest.TestCase):
+    """test for analysing the numberical columns"""
+
+    def setUp(self):
+        """Set up sample data for testing."""
+        self.data = pd.Series(
+            [10, 20, 30, None, "nan", "Nan", "", None, 40, 50])
+
+    def test_anlz_numerical_cols_summary(self):
+        """
+        Test if anlz_numerical_cols correctly calculates the
+        summary statistics.
+        """
+        summary, _ = anlz_numerical_cols(self.data)
+        expected_summary = pd.DataFrame({
+            "Total": [10],
+            "Valid": [5],
+            "Missing": [5],
+            "Mean": [30.0],
+            "Std Dev": [15.811388300841896],
+            "Min": [10.0],
+            "Max": [50.0]
+        })
+        pd.testing.assert_frame_equal(summary, expected_summary)
+
+    def test_anlz_numerical_cols_descriptive_stats(self):
+        """
+        Test if anlz_numerical_cols correctly calculates
+        descriptive statistics.
+        """
+        _, descriptive_stats = anlz_numerical_cols(self.data)
+        expected_descriptive_stats = pd.Series({
+            "count": 5.0,
+            "mean": 30.0,
+            "std": 15.811388300841896,
+            "min": 10.0,
+            "25%": 20.0,
+            "50%": 30.0,
+            "75%": 40.0,
+            "max": 50.0
+        })
+        pd.testing.assert_series_equal(descriptive_stats,
+                                       expected_descriptive_stats)
 
 
 if __name__ == "__main__":
