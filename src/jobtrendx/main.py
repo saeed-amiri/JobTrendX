93d998e8a2551f663763735a2554d5dd27570504
saeed amiri <sdramiri952@gmail.com>
2025-04-24 19:05:34 +0200

    set the languages

--- src/jobtrendx/main.py
+++ src/jobtrendx/main.py
@@ -43,6 +43,7 @@ def main(cfg: DictConfig) -> None:
     df_i: pd.DataFrame = anlaz.df_info
 
     df_cleaned: pd.DataFrame = clean_dataframe.remove_duplicate(df_info=df_i)
+    df_cleaned: pd.DataFrame = clean_dataframe.set_languages(df=df_cleaned)
 
     stats = statistics.StatisticsManager(df_info=df_cleaned)
     stats.statistics(log=LOG)
