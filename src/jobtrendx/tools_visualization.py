f94cafad380df4c26f4b39dcb9e0319a8a1cdf30
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2025-04-26 19:26:21 +0200

    update plot (#33)
    * update the names
    
    * update plot
    
    * plot the skills

--- src/jobtrendx/tools_visualization.py
+++ src/jobtrendx/tools_visualization.py
@@ -8,38 +8,33 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 
 
-def plot_job_titles(job_counts: pd.Series,
-                    threshold: float = 0.03
-                    ) -> None:
+def plot_counts_series(counts: pd.Series,
+                       threshold: float = 0.03,
+                       data_name: str = 'counts'
+                       ) -> None:
     """
-    Plot a professional pie chart for job titles, grouping
+    Plot a professional pie chart for counts, grouping
     minor categories into 'Other'.
 
     Parameters:
     -----------
-    job_counts : pd.Series
+    counts : pd.Series
         Series with job titles as index and counts as values.
     threshold : float
         Minimum fraction to display separately. Others will
         be grouped.
     """
     # Normalize and group small slices
-    total = job_counts.sum()
-    fraction = job_counts / total
-    mask = fraction < threshold
-
-    major = job_counts[~mask].copy()
-    minor = job_counts[mask]
-
     other_label = "Other:"
-    grouped = major.copy()
-    if not minor.empty:
-        grouped[other_label] = minor.sum()
+    total = counts.sum()
 
-    # Colors
-    n = len(grouped)
-    colors = sns.color_palette("Set3", n_colors=n).as_hex()
-    explode = [0.05] * n
+    grouped, minor = _normalize_others(counts=counts,
+                                       threshold=threshold,
+                                       other_label=other_label)
+
+    length: int = len(grouped)
+    explode = [0.05] * length
+    colors = _set_colors(length=length)
 
     fig, ax = plt.subplots(figsize=(10, 10))
     wedges, _, autotexts = ax.pie(
@@ -49,36 +44,85 @@ def plot_job_titles(job_counts: pd.Series,
         explode=explode,
         autopct=lambda pct: f"{pct:.1f}%\n({int(round(pct * total / 100))})",
         pctdistance=0.8,
-        startangle=140
+        startangle=85
     )
 
+    explode = [0.05] * length
+
     # Legend items
-    handles = []
-    labels = []
-
-    for i, title in enumerate(grouped.index):
-        if title == other_label:
-            # Add colored "Other:" label
-            handles.append(Patch(facecolor=colors[i], label=title))
-            labels.append(title)
-            # Add plain text sub-items without color boxes
-            for sub in minor.index[:10]:
-                handles.append(Patch(facecolor='none', edgecolor='none'))
-                labels.append(f"  - {sub}")
-        else:
-            handles.append(Patch(facecolor=colors[i], label=title))
-            labels.append(title)
-
+    handles, labels = _handle_legend(grouped=grouped,
+                                     minor=minor,
+                                     colors=colors,
+                                     other_label=other_label)
     ax.legend(
         handles,
         labels,
-        title="Job Titles",
+        title=None,
         loc="center left",
         bbox_to_anchor=(1, 0.5),
         frameon=False
     )
 
-    ax.set_title("Distribution of Job Titles", fontsize=16, weight='bold')
+    ax.set_title(f"Distribution of {data_name.capitalize()}",
+                 fontsize=16,
+                 weight='bold')
+    _save_fig(fig=fig, data_name=data_name)
+
+
+def _normalize_others(counts: pd.Series,
+                      threshold: float,
+                      other_label: str
+                      ) -> tuple[pd.Series, pd.Series]:
+    """Nomarilze and group the small slices"""
+    total = counts.sum()
+    fraction = counts / total
+    mask = fraction < threshold
+
+    major = counts[~mask].copy()
+    minor = counts[mask]
+
+    grouped = major.copy()
+    if not minor.empty:
+        grouped[other_label] = minor.sum()
+    return grouped, minor
+
+
+def _set_colors(length: int,
+                style: str = "Set3"
+                ) -> list:
+    """set the color of the pie"""
+    return sns.color_palette(style, n_colors=length).as_hex()
+
+
+def _handle_legend(grouped: pd.Series,
+                   minor: pd.Series,
+                   colors: list[str],
+                   other_label: str
+                   ) -> tuple[list, list]:
+    handles: list = []
+    labels: list = []
+    for i, item in enumerate(grouped.index):
+        if item == other_label:
+            # Add colored "Other:" label
+            handles.append(Patch(facecolor=colors[i], label=item))
+            labels.append(item)
+            # Add plain text sub-items without color boxes
+            for sub in minor.index[:10]:
+                handles.append(Patch(facecolor='none', edgecolor='none'))
+                labels.append(f"  - {sub}")
+            if len(minor.index >= 10):
+                handles.append(Patch(facecolor='none', edgecolor='none'))
+                labels.append(f"  - {'...'}")
+        else:
+            handles.append(Patch(facecolor=colors[i], label=item))
+            labels.append(item)
+    return handles, labels
+
+
+def _save_fig(fig,
+              data_name: str
+              ) -> None:
+    """save figure"""
     plt.tight_layout()
-    fig.savefig(fname='job_title.jpeg')
-    plt.show()
+    fout: str = data_name.replace(' ', '_')
+    fig.savefig(fname=f'{fout}.jpeg')
