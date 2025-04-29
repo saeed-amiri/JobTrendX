bd57ee5919d3592c6b055c10dcadebed228020ce
saeed amiri <sdramiri952@gmail.com>
2025-04-29 17:24:12 +0200

    update class args

--- src/jobtrendx/statistics.py
+++ src/jobtrendx/statistics.py
@@ -19,7 +19,8 @@ class StatisticsManager:
         'skills_count',
         'lang_count',
         'salary_min',
-        'salary_max'
+        'salary_max',
+        'log'
     ]
 
     df_info: pd.DataFrame
@@ -28,58 +29,51 @@ class StatisticsManager:
     lang_count: pd.Series
     salary_min: pd.Series
     salary_max: pd.Series
+    log: logger.logging.Logger
 
     def __init__(self,
                  df_info: pd.DataFrame,
+                 log: logger.logging.Logger
                  ) -> None:
         self.df_info = df_info
+        self.log = log
 
-    def statistics(self,
-                   log: logger.logging.Logger
-                   ) -> None:
+    def statistics(self) -> None:
         """call the methods and set the objects"""
-        self._analyze_job_titles(log)
-        self._analyze_skills(log)
-        self._analyze_languages(log)
-        self._analyze_salaries(log)
+        self._analyze_job_titles()
+        self._analyze_skills()
+        self._analyze_languages()
+        self._analyze_salaries()
 
-    def _analyze_job_titles(self,
-                            log: logger.logging.Logger
-                            ) -> None:
+    def _analyze_job_titles(self) -> None:
         """analyzing the job titles"""
         summary: pd.DataFrame
         summary, self.job_title_top = \
             tools.anlz_string_cols(self.df_info['job_title'])
 
-        log.info(f'\n\nJob title summary:\n{summary}'
+        self.log.info(f'\n\nJob title summary:\n{summary}'
                  f'\n\n{self.job_title_top}\n')
 
-    def _analyze_skills(self,
-                        log: logger.logging.Logger
-                        ) -> None:
+    def _analyze_skills(self) -> None:
         """analysis the skills"""
         summary: pd.DataFrame
         summary, self.skills_count = tools.anlz_list_cols(self.df_info.skills)
 
-        log.info(f'\n\nSkills summary:\n{summary}'
+        self.log.info(f'\n\nSkills summary:\n{summary}'
                  f'\n\n{self.skills_count.head(8)}\n')
 
-    def _analyze_languages(self,
-                           log: logger.logging.Logger
-                           ) -> None:
+    def _analyze_languages(self) -> None:
         """analysis the skills"""
         summary: pd.DataFrame
         summary, self.lang_count = tools.anlz_list_cols(self.df_info.language)
 
-        log.info(f'\n\nLanguages summary:\n{summary}'
+        self.log.info(f'\n\nLanguages summary:\n{summary}'
                  f'\n\n{self.lang_count.head(8)}\n')
 
-    def _analyze_salaries(self,
-                          log: logger.logging.Logger
-                          ) -> None:
+    def _analyze_salaries(self) -> None:
         """analyze salary columns"""
         for col_name in ['salary_min', 'salary_max']:
             summary, attr_value = \
                 tools.anlz_numerical_cols(self.df_info[col_name])
             setattr(self, col_name, attr_value)
-            log.info(f'\n\n{col_name.capitalize()} Summary:\n{summary}')
+            self.log.info(f'\n\n{col_name.capitalize()} Summary:\n{summary}')
